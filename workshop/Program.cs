cd1f1810c32633f78b122898f0a2eb3a086eb1a9
Elena <am1ks@yandex.ru>
2022-08-29 00:12:03 +0300

    HomeWork2

---
+++ workshop/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача № 1
+// int Twonumber(int number){
+//     return number /10 % 10;
+// }
+// Random rnd = new Random();
+// int n = rnd.Next(100, 1000);
+// System.Console.WriteLine($"Вторая цифра числа {n} - {Twonumber(n)}");
+
+//Задача № 2
+// int Threenumber(int number){
+//     if (number < 100){
+//         System.Console.WriteLine("Третьей цифры нет");
+//         return 0;
+//     }
+//     int result = 0;
+//     while (number > 999){
+//         number /= 10;
+//         result = number % 10;
+//     }
+//     return result;
+// }
+// System.Console.WriteLine(Threenumber(43421221));
+
+//Задача № 3
+
+bool Weekend(int number){
+    if(number > 7 || number < 1){
+        return false;
+    }
+    if (number > 5){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+System.Console.WriteLine(Weekend(5));
+
+
+
